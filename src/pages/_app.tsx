8f70e164df1993c8b2924d0dfad4e5d0c700be61
Denton Flake <me@dentonflake.com>
2025-06-18 10:41:26 -0700

    Imported Montserrat font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    variable: '--font-montserrat',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <main className={montserrat.variable}>
+            <Component {...pageProps} />;
+        </main>
+    );
 }
