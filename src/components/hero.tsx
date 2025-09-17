05ca90c125741303bcce590f85dd0b52100d0813
Denton Flake <me@dentonflake.com>
2025-09-17 15:40:08 -0700

    Fixed hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,17 +6,19 @@ const Hero = () => {
   return (
     <section className={styles.hero}>
 
-      <Image
-        className={styles.image}
-        src="/images/profile.png"
-        alt="Memoji of Denton Flake"
-        width={300}
-        height={300}
-        priority
-      ></Image>
-
       <div className={styles.content}>
+
+        <Image
+          className={styles.image}
+          src="/images/profile.png"
+          alt="Memoji of Denton Flake"
+          width={300}
+          height={300}
+          priority
+        ></Image>
+
         <h1 className={styles.title}>Hello! I&apos;m <span className={styles.name}>Denton Flake</span></h1>
+
       </div>
 
     </section>
