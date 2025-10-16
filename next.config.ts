8b2137e2cb132fe97a640583d33ae8c7ee83fc42
Denton Flake <me@dentonflake.com>
2025-10-16 15:20:17 -0700

    Removed config options

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
 };
 
 export default nextConfig;
